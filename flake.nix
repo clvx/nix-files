2591db35c91ff6e6e2f1b5599f3ff28d74a42341
Luis Michael Ibarra <michael.ibarra@gmail.com>
2023-06-24 10:47:11 -0600

    adding support for mac

--- flake.nix
+++ flake.nix
@@ -14,9 +14,15 @@
         defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
 
         homeConfigurations = {
-            "clvx" = home-manager.lib.homeManagerConfiguration {
+            "void" = home-manager.lib.homeManagerConfiguration {
                 # Note: I am sure this could be done better with flake-utils or something
                 pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+                modules = [ ./home.nix ];
+            };
+            "abyss" = home-manager.lib.homeManagerConfiguration {
+                # Note: I am sure this could be done better with flake-utils or something
+                pkgs = nixpkgs.legacyPackages.aarch64-darwin;
 
                 modules = [ ./home.nix ];
             };
