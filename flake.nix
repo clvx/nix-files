b18c93fd0f2bf82360327177bdb95f3602399255
Luis Michael Ibarra <michael.ibarra@gmail.com>
2023-06-24 11:09:16 -0600

    formatting

--- flake.nix
+++ flake.nix
@@ -1,31 +1,28 @@
 {
-    description = "My Home Manager Flake";
+  description = "My Home Manager Flake";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = {nixpkgs, home-manager, ...}: {
-        defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-        defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+  outputs = {nixpkgs, home-manager, ...}: {
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+    defaultPackage.x86_64-darwin = home-manager.defaultPackage.aarch64-darwin;
 
-        homeConfigurations = {
-            "void" = home-manager.lib.homeManagerConfiguration {
-                # Note: I am sure this could be done better with flake-utils or something
-                pkgs = nixpkgs.legacyPackages.x86_64-linux;
-
-                modules = [ ./home.nix ];
-            };
-            "abyss" = home-manager.lib.homeManagerConfiguration {
-                # Note: I am sure this could be done better with flake-utils or something
-                pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-
-                modules = [ ./home.nix ];
-            };
-        };
+    homeConfigurations = {
+      "void" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [ ./home.nix ];
+      };
+      "abyss" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+          modules = [ ./home.nix ];
+      };
     };
+  };
 }
