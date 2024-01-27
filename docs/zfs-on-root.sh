feb93cf9a639e0f40151069c6c31b0c5cc0858f8
clvx <michael.ibarra@gmail.com>
2024-01-27 08:44:38 -0700

    refactoring for systemd-boot

--- docs/zfs-on-root.sh
+++ docs/zfs-on-root.sh
@@ -1,7 +1,8 @@
 #https://openzfs.github.io/openzfs-docs/Getting%20Started/NixOS/Root%20on%20ZFS.html
 
 #DISK='/dev/disk/by-id/ata-FOO /dev/disk/by-id/nvme-BAR'
-DISK=''
+DISK='/dev/disk/by-id/nvme-eui.002538b81150152a /dev/disk/by-id/nvme-eui.002538ba115083f3'
+
 
 echo "Creating Mount dir"
 MNT=$(mktemp -d)
@@ -25,16 +26,11 @@ partition_disk () {
  parted --script --align=optimal  "${disk}" -- \
  mklabel gpt \
  mkpart EFI 2MiB 1GiB \
- mkpart bpool 1GiB 5GiB \
  mkpart rpool 5GiB -$((SWAPSIZE + RESERVE))GiB \
  mkpart swap  -$((SWAPSIZE + RESERVE))GiB -"${RESERVE}"GiB \
- mkpart BIOS 1MiB 2MiB \
  set 1 esp on \
- set 5 bios_grub on \
- set 5 legacy_boot on
 
  partprobe "${disk}"
- udevadm settle
 }
 
 echo "partitioning disk"
@@ -44,38 +40,17 @@ done
 
 echo "creating encrypted swap"
 for i in ${DISK}; do
-   cryptsetup open --type plain --key-file /dev/random "${i}"-part4 "${i##*/}"-part4
-   mkswap /dev/mapper/"${i##*/}"-part4
-   swapon /dev/mapper/"${i##*/}"-part4
+   cryptsetup open --type plain --key-file /dev/random "${i}"-part3 "${i##*/}"-part3
+   mkswap /dev/mapper/"${i##*/}"-part3
+   swapon /dev/mapper/"${i##*/}"-part3
 done
 
 echo "enabling Luks on root"
 for i in ${DISK}; do
    # see PASSPHRASE PROCESSING section in cryptsetup(8)
-   printf "YOUR_PASSWD" | cryptsetup luksFormat --type luks2 "${i}"-part3 -
-   printf "YOUR_PASSWD" | cryptsetup luksOpen "${i}"-part3 luks-rpool-"${i##*/}"-part3 -
+   printf "PASSWD" | cryptsetup luksFormat --type luks2 "${i}"-part2 -
+   printf "PASSWD" | cryptsetup luksOpen "${i}"-part2 luks-rpool-"${i##*/}"-part2 -
 done
-
-
-echo "creating bpool"
-# shellcheck disable=SC2046
-#
-  #mirror \ # Add this line after bpool
-zpool create -o compatibility=legacy  \
-    -o ashift=12 \
-    -o autotrim=on \
-    -O acltype=posixacl \
-    -O canmount=off \
-    -O devices=off \
-    -O normalization=formD \
-    -O relatime=on \
-    -O xattr=sa \
-    -O mountpoint=/boot \
-    -R "${MNT}" \
-    bpool \
-    $(for i in ${DISK}; do
-       printf '%s ' "${i}-part2";
-      done)
 
 echo "creating rpool"
 # shellcheck disable=SC2046
@@ -86,41 +61,33 @@ zpool create \
     -R "${MNT}" \
     -O acltype=posixacl \
     -O canmount=off \
-    -O compression=zstd \
     -O dnodesize=auto \
     -O normalization=formD \
     -O relatime=on \
     -O xattr=sa \
-    -O mountpoint=/ \
+    -O mountpoint=none \
     rpool \
+    mirror \
    $(for i in ${DISK}; do
-      printf '/dev/mapper/luks-rpool-%s ' "${i##*/}-part3";
+      printf '/dev/mapper/luks-rpool-%s ' "${i##*/}-part2";
      done)
 
 
-echo "mounting rpool"
-zfs create -o mountpoint=legacy -p rpool/nixos/root
-mount -t zfs rpool/nixos/root "${MNT}"/
-zfs create -o mountpoint=legacy rpool/nixos/home
-mkdir "${MNT}"/home
-mount -t zfs rpool/nixos/home "${MNT}"/home
-zfs create -o mountpoint=none   rpool/nixos/var
-zfs create -o mountpoint=legacy rpool/nixos/var/lib
-zfs create -o mountpoint=legacy rpool/nixos/var/log
-zfs create -o mountpoint=none bpool/nixos
-zfs create -o mountpoint=legacy bpool/nixos/root
-mkdir "${MNT}"/boot
-mount -t zfs bpool/nixos/root "${MNT}"/boot
-mkdir -p "${MNT}"/var/log
-mkdir -p "${MNT}"/var/lib
-mount -t zfs rpool/nixos/var/lib "${MNT}"/var/lib
-mount -t zfs rpool/nixos/var/log "${MNT}"/var/log
-zfs create -o mountpoint=legacy rpool/nixos/empty
-zfs snapshot rpool/nixos/empty@start
+echo "create root system container"
+zfs create -o canmount=noauto -o mountpoint=legacy -p rpool/root
 
-echo "mounting bpool"
+
+echo "create system datasets"
+zfs create -o mountpoint=legacy rpool/home
+mount -o X-mount.mkdir -t zfs rpool/root "${MNT}"
+mount -o X-mount.mkdir -t zfs rpool/home "${MNT}"/home
+
+echo "format and mount ESP - this needs mirroring as only one is used as /boot"
 for i in ${DISK}; do
- mkfs.vfat -n EFI "${i}"-part1
- mkdir -p "${MNT}"/boot/efis/"${i##*/}"-part1
- mount -t vfat -o iocharset=iso8859-1 "${i}"-part1 "${MNT}"/boot/efis/"${i##*/}"-part1
+  mkfs.vfat -n EFI "{i}"-part1
+done
+
+for i in ${DISK}; do
+  mount -t vfat -o fmask=0077,dmask=0077,iocharset=iso8859-1,X-mount.mkdir "${i}"-part1 "${MNT}"/boot
+  break
 done
