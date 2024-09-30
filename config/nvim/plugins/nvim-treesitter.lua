b8ecb1bf71fb4a99b5a7675338206ebd7dac38e1
clvx <michael.ibarra@gmail.com>
2024-09-30 07:30:49 -0600

    disabling autoupdate as fzf-lua and others cannot write to nix store/add text context

--- config/nvim/plugins/nvim-treesitter.lua
+++ config/nvim/plugins/nvim-treesitter.lua
@@ -16,12 +16,14 @@ require'nvim-treesitter.configs'.setup {
   --ensure_installed = "maintained",
   ensure_installed = "",
 
+
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+  -- DISABLING THIS FUCKER AS IT COLLIDES WITH NIX.
+  auto_install = false,
 
   -- List of parsers to ignore installing (for "all")
   --ignore_install = { "javascript" },
@@ -34,5 +36,11 @@ require'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
+  },
+  textobjects = {
+      select = {
+          enable = true,
+          lookahead = true
+      }
   },
 }
